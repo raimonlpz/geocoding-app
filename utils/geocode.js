ddb27050dac55f24f59af91c2d9b2f1ed7285241
Raimon López <59086203+raimonlpz@users.noreply.github.com>
2020-03-27 23:18:13 +0100

    Update geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -5,7 +5,7 @@ const request = require("request");
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicmFpbW9uMTIzIiwiYSI6ImNrOGEwbDVlNzA2aGUzbGxvMnVkZWV6YjkifQ.HobSv-zMpbkwG59UGJx_SQ&limit=1`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, response) => {
     if (error) {
       callback("Unable to connect to location services...", undefined);
     } else if (response.body.features.length === 0) {
